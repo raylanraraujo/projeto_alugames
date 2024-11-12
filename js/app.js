f570975c31e269c4f5e565b70a0579c482ffdbfc
Raylan Araujo <raylanra@gmail.com>
2024-11-12 19:12:30 -0300

    Criar variáveis para recuperar as informações

--- js/app.js
+++ js/app.js
@@ -1,4 +1,9 @@
 function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`); //recupera o elemento pelo ID onde na chamada de função é passado apenas o número
+    let imagem = gameClicado.querySelector('.dashboard__item__img'); //recupera o elemento div (imagem) do jogo clicado
+    let botao = gameClicado.querySelector('.dashboard__item__button'); // recupera o botão (tag <a>) do jogo clicado
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name'); //recupera o nome do jogo clicado
 
+    alert(nomeJogo.textContent) //configura um alerta que retorna o texto da tag
 }
 
