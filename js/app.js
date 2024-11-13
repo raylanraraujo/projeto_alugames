1bbbe2a45705cb985d97ddc91d4026779478526c
Raylan Araujo <raylanra@gmail.com>
2024-11-13 06:42:20 -0300

    Criar condicional para alterar status dos jogos

--- js/app.js
+++ js/app.js
@@ -2,8 +2,18 @@ function alterarStatus(id) {
     let gameClicado = document.getElementById(`game-${id}`); //recupera o elemento pelo ID onde na chamada de função é passado apenas o número
     let imagem = gameClicado.querySelector('.dashboard__item__img'); //recupera o elemento div (imagem) do jogo clicado
     let botao = gameClicado.querySelector('.dashboard__item__button'); // recupera o botão (tag <a>) do jogo clicado
-    let nomeJogo = gameClicado.querySelector('.dashboard__item__name'); //recupera o nome do jogo clicado
+    let nomeJogo = gameClicado.querySelector('.dashboard__item__name'); //recupera o nome do jogo clicado --> foi usado somente para teste. Pode ser apagado
 
-    alert(nomeJogo.textContent) //configura um alerta que retorna o texto da tag
+    //eu tinha feito 2 if/else ( um para a image e outro para o botao)
+    //mas é possível colocar tudo junto
+    
+    if (imagem.classList.contains('dashboard__item__img--rented')) {  //para os jogos que estavam alugados
+        imagem.classList.remove('dashboard__item__img--rented');
+        botao.classList.remove('dashboard__item__button--return');
+        botao.innerHTML = 'Alugar'
+    } else {    //para os jogos que estavam disponíveis
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.classList.add('dashboard__item__button--return');
+        botao.innerHTML = 'Devolver'
+    }
 }
-
